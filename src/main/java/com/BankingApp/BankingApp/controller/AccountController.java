49c9868b92d476d2cd0df41b7a68f020e640be33
mindedboys <rohit.vrm888@gmail.com>
2024-04-21 18:56:04 +0530

    add CRUD Method in controller

--- src/main/java/com/BankingApp/BankingApp/controller/AccountController.java
+++ src/main/java/com/BankingApp/BankingApp/controller/AccountController.java
@@ -5,10 +5,10 @@ import com.BankingApp.BankingApp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/accounts")
@@ -27,5 +27,52 @@ public class AccountController {
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
         System.out.println("accountDto :-" + accountDto);
         return new ResponseEntity<>(accountService.CreateAccount(accountDto), HttpStatus.CREATED);
+    }
+
+    //http://localhost:8080/api/accounts/4
+    @GetMapping("/{id}") // To Get Account Details Rest API
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
+        AccountDto accountDto = accountService.getAccountById(id);
+    return ResponseEntity.ok(accountDto);
+    }
+
+    //http://localhost:8080/api/accounts/3/deposit
+   // {
+    //    "amount":2000
+   // }
+    @PutMapping("/{id}/deposit")  // Deposit Amount in account Rest API
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id, @RequestBody Map<String,Double> request){
+       Double amount = request.get("amount");
+       AccountDto accountDto = accountService.deposit(id, amount);
+    return ResponseEntity.ok(accountDto);
+    }
+
+
+    //http://localhost:8080/api/accounts/4/withdraw
+    /*
+    {
+    "amount":400.0
+    }
+    */
+    @PutMapping("/{id}/withdraw") // Withdraw amount in account Rest API
+    public ResponseEntity<AccountDto> withdraw(@PathVariable Long id, @RequestBody Map<String, Double> request){
+       Double amount = request.get("amount");
+       AccountDto accountDto = accountService.withdraw(id , amount);
+    return ResponseEntity.ok(accountDto);
+    }
+
+
+    // http://localhost:8080/api/accounts
+    @GetMapping // To Get All Accounts details Rest API
+    public ResponseEntity<List <AccountDto>> getAllAccount(){
+      List<AccountDto> accountDto = accountService.getAllAccounts();
+    return ResponseEntity.ok(accountDto);
+    }
+
+    // http://localhost:8080/api/accounts/4
+    @DeleteMapping("/{id}") // Delete Account Rest API
+    public ResponseEntity<String> deleteAccount(@PathVariable Long id){
+          accountService.deleteAccount(id);
+    return ResponseEntity.ok("Account deleted Successfully ...!!");
     }
 }
